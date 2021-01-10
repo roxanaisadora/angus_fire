50dad0b24f4f647b0905d9d2b141bb3c20c6e6d5
roxana isadora <roxanaisadora@gmail.com>
2021-01-09 21:13:32 -0500

    update of styles and js shipping

--- frontend/js/shopping.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-    const{value:name} = await Swal.fire({
-    title: 'Bienvenido !!!',
-    input: 'text',
-    inputLabel: 'Your firts name',
-    inputPlaceholder: 'Enter your firts name'
-});
-document.getElementById('confirmation').innerHTML = `Welcome, ${name} nice to have you here 😊`;
-})()
--- public/js/UIShopping.js
+++ public/js/UIShopping.js
@@ -1,20 +1,26 @@
 import PurchasService from '../services/PurchasService.js';
+import SurveyService from '../services/SurveyService.js'
+const surveyService = new SurveyService()
 const purchasService = new PurchasService()
 
 class UIShopping {
     async render(){
+        const surveys = await surveyService.getSurvey()
         const shoppings = await purchasService.getPurchas();
         const shoppingCard = document.getElementById('purchas-cards');
-        shoppingCard.innerHTML = '';
+        const formcards = document.getElementById('form-cards');
         document.getElementById('form-email')
                 .addEventListener('submit', e => {
                     const email = document.getElementById('email').value;
-                    console.log(email)
+                    //console.log(email)
                     e.preventDefault();
+                    formcards.innerHTML = '';
+                    shoppingCard.innerHTML = '';
                     shoppings.forEach((purchas) => {
                             if(email == purchas.email){
                                 const div = document.createElement('div');
                                 div.className = 'contend';
+                                div.id = 'contend';
                                 div.innerHTML = `
                                 <div class="card">
                                     <div class="col-4">
@@ -31,21 +37,38 @@ class UIShopping {
                                 </div>
                                 `;
                                 shoppingCard.appendChild(div);
-                            }else{
-                                console.log('no te encontre')
-                                const div = document.createElement('div');
-                                div.className = 'contended';
-                                div.innerHTML = `
-                                `;
-                                shoppingCard.appendChild(div);
                             }
-                      });
+
+                    });
+                    surveys.forEach((survey) => {
+                            if(email == survey.email){
+                                const div = document.createElement('div');
+                                div.className = 'contend-1';
+                                div.innerHTML = `
+                                <div class="card-1">
+                                        <div class="data-1">
+                                            <h4>Name: ${survey.name}</h4>
+                                            <h4>date: ${survey.created_at}</h4>
+                                            <p>Description: ${survey.comments}</p>
+                                        </div>
+                                </div>
+                                `;
+                                formcards.appendChild(div);
+                            }
+                    });
                 })
-        
     }
     async deletePurchas(purchasId) {
         await purchasService.deletePurchas(purchasId)
-        setTimeout("location.reload(true);",1500)
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'The purchase has been deleted',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        const shoppingCard = document.getElementById('purchas-cards');
+        shoppingCard.innerHTML = '';
         this.render();
         }
 }
