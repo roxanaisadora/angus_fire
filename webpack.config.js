c84a3a2b25b1cbba1bd419ddc051237e2fd1d82d
roxana isadora <roxanaisadora@gmail.com>
2021-01-09 22:32:35 -0500

    update for deploy

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    entry: './frontend/js/confirm.js',
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'backend/public'),
+        filename: 'js/index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './frontend/index.html',
+        minify: {
+            collapseWhitespace: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true
+        }
+        })
+    ]
+};
